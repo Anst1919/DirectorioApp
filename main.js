def720107933b05fdd882ea341e30335824df80d
Ashly Naomi <naosanchezt@gmail.com>
2025-05-15 19:59:38 -0600

    Tarea1-1ER-PARCIAL
    Se consumió otra API y se mostraron los datos en la consola, todo ello fue realizado en el archivo main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// EN JS tenemos las funciones de tipo flecha
+// En JS tenemos las funciones de tipo flecha
 // Utilizaremos fetch que es un método para consumir una API
 
 //Paso 1: Función flecha
@@ -7,9 +7,24 @@ const consumirApi = () => {
 //Convertimos la respuesta a tipo JSON
     .then((response) => response.json ())
 //Los datos los vamos a mandar a la consola
-.then((data) => console.log(data))
+.then((data) => console.log("USERS:", data))
 //Descubrir qué hacer en caso de que no me corresponda
 .catch((error) => console.log(error))
 };
 
 consumirApi();
+
+// Function to consume the "posts" API
+const fetchPosts = () => {
+  // Get data from the API
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    // Convert the response to JSON format
+    .then((response) => response.json())
+    // Display the data in the console
+    .then((data) => console.log("POSTS:", data))
+    // Handle possible errors
+    .catch((error) => console.log("ERROR:", error));
+};
+
+// Call the function
+fetchPosts();
